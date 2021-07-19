09352ff2ebf329f5addb3478e1abbfb0af2d41d0
Artem <pasechnik@1060.ru>
2021-07-19 16:12:54 +0300

    initial commit with qrcode recognition function

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from pyzbar import pyzbar
+import cv2
+def draw_barcode(decoded, image):
+    # n_points = len(decoded.polygon)
+    # for i in range(n_points):
+    #     image = cv2.line(image, decoded.polygon[i], decoded.polygon[(i+1) % n_points], color=(0, 255, 0), thickness=5)
+    # раскомментируйте выше и закомментируйте ниже, если хотите нарисовать многоугольник, а не прямоугольник
+    image = cv2.rectangle(image, (decoded.rect.left, decoded.rect.top),
+                            (decoded.rect.left + decoded.rect.width, decoded.rect.top + decoded.rect.height),
+                            color=(0, 255, 0),
+                            thickness=5)
+    return image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def decode(image):
+    # decodes all barcodes from an image
+    decoded_objects = pyzbar.decode(image)
+    for obj in decoded_objects:
+        # draw the barcode
+        print(f"Обнаружен штрих-код:\n{obj}")
+        image = draw_barcode(obj, image)
+        # print barcode type & data
+        print("Тип:", obj.type)
+        print("Данные:", obj.data)
+        print()
+    return image
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+imgc=cv2.imread("C:/Users/artem/Downloads/Telegram Desktop/photo_2021-07-19_15-57-40.jpg")
+imgc=decode(imgc)
+cv2.imshow('img',imgc)
+cv2.waitKey(0)
